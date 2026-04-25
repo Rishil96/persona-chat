70b5a78546efcb24aa9ede507d1869d12dad917d
Rishil Ramesh <rishil.ramesh@outlook.com>
2026-04-25 21:53:23 +0530

    Add : Logic to load environment variables and create local database in main module

--- persona-backend/app/main.py
+++ persona-backend/app/main.py
@@ -1,5 +1,13 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from app.db.database import Base, get_engine
 
+# Load environment variables
+load_dotenv()
+
+# Database setup
+engine = get_engine()
+Base.metadata.create_all(engine)
 
 app = FastAPI(title="Persona")
 
