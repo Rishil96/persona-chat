f1747f4b7efc078f6e7c3a5e4cf17550e52ec207
Rishil Ramesh <rishil.ramesh@outlook.com>
2026-04-25 21:52:26 +0530

    Change : Convert engine and sessionmaker into functions and add model imports in __init__.py for Base to find models while creation

--- persona-backend/app/db/database.py
+++ persona-backend/app/db/database.py
@@ -3,9 +3,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
-engine = create_engine(url=DATABASE_URL, connect_args={'check_same_thread': False})
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+# Engine function
+def get_engine():
+    """
+    Function to create and return a SQLAlchemy Engine object
+    """
+    url = os.environ.get("DATABASE_URL")
+    return create_engine(url=url, connect_args={'check_same_thread': False})
+
+# Session maker function
+def get_session(engine):
+    return sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 
 # Base class for all ORM models
