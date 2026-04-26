50aea64bb015abb0d8a31e72e157dbb216803ac5
Rishil Ramesh <rishil.ramesh@outlook.com>
2026-04-26 17:42:12 +0530

    Add : Logic to instantiate engine and session at global level in main module

--- persona-backend/app/main.py
+++ persona-backend/app/main.py
@@ -1,12 +1,13 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
-from app.db.database import Base, get_engine
+from app.db.database import Base, get_engine, get_session
 
 # Load environment variables
 load_dotenv()
 
 # Database setup
 engine = get_engine()
+SessionLocal = get_session()
 Base.metadata.create_all(engine)
 
 app = FastAPI(title="Persona")
