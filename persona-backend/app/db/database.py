0b9313ee31adf1041976608a707c62cdc4280932
Rishil Ramesh <rishil.ramesh@outlook.com>
2026-04-26 17:39:34 +0530

    Add : Logic to implement engine and session as singleton objects

--- persona-backend/app/db/database.py
+++ persona-backend/app/db/database.py
@@ -3,17 +3,29 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 
+# Variables for singleton instantiation of engine and sessionmaker
+engine = None
+SessionLocal = None
+
 # Engine function
 def get_engine():
     """
     Function to create and return a SQLAlchemy Engine object
     """
-    url = os.environ.get("DATABASE_URL")
-    return create_engine(url=url, connect_args={'check_same_thread': False})
+    global engine
+    if engine:
+        return engine
+    url = os.environ.get("DATABASE_URL", "sqlite:///./persona.db")
+    engine = create_engine(url=url, connect_args={'check_same_thread': False})
+    return engine
 
 # Session maker function
-def get_session(engine):
-    return sessionmaker(bind=engine, autoflush=False, autocommit=False)
+def get_session():
+    global engine, SessionLocal
+    if SessionLocal:
+        return SessionLocal
+    SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+    return SessionLocal
 
 
 # Base class for all ORM models
