96bebc3590f8734941fa111f2e8e48bcd99e9b81
Rishil Ramesh <rishil.ramesh@outlook.com>
2026-04-29 21:53:26 +0530

    Add : Logger package to create singleton logger

---
+++ persona-backend/app/logger/logger.py
@@ -0,0 +1,15 @@
+import logging
+
+
+def setup_logger():
+    """
+    Singleton logger setup helper function
+    """
+    logger = logging.getLogger("persona")
+    logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
