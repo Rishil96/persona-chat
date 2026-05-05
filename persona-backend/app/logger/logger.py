17d63fcb125115931ca14a152aee7a379fe223d8
Rishil Ramesh <rishil.ramesh@outlook.com>
2026-05-05 16:57:38 +0530

    Add : get_logger function to handle singleton logger creation

--- persona-backend/app/logger/logger.py
+++ persona-backend/app/logger/logger.py
@@ -3,14 +3,15 @@ import os
 from app.constants import LOGGER_NAME, DEFAULT_LOGGER
 
 
-def setup_logger():
+def get_logger():
     """
-    Singleton logger setup helper function
+    Function to create and return a singleton logger
     """
     logger = logging.getLogger(os.getenv(LOGGER_NAME, DEFAULT_LOGGER))
-    logger.setLevel(logging.DEBUG)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    if not logger.handlers:
+        logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
     return logger
--- persona-backend/app/main.py
+++ persona-backend/app/main.py
@@ -1,19 +1,11 @@
-import logging
-import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from app.db.database import Base, get_engine, get_session
-from app.logger import setup_logger
 from app.routers.conversations import router as conversation_router
 from app.routers.chat import router as chat_router
-from app.constants import LOGGER_NAME, DEFAULT_LOGGER
 
 # Load environment variables
 load_dotenv()
-
-# Setup singleton logger
-setup_logger()
-logger = logging.getLogger(os.getenv(LOGGER_NAME, DEFAULT_LOGGER))
 
 # Database setup
 engine = get_engine()
