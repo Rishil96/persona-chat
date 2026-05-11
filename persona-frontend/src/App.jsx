372fe48322360528e7dea5644b35ebb9a23e120f
Rishil Ramesh <rishil.ramesh@outlook.com>
2026-05-11 20:56:15 +0530

    Add : Conversation ID useState at App level

--- persona-frontend/src/App.jsx
+++ persona-frontend/src/App.jsx
@@ -1,20 +1,19 @@
 import './App.css'
 import Sidebar from './components/Sidebar.jsx'
+import { useState } from 'react'
 
 function App() {
-
-  return (
-    <>
-      <div className="flex h-screen">
-        <div className="w-64 bg-gray-900">
-          <Sidebar/>
-        </div>
-        <div className="flex-1 bg-gray-50">
-          Chat Window Placeholder
-        </div>
-      </div>
-    </>
-  )
+	const [conversationID, setConversationID] = useState(null)
+	return (
+		<div className="flex h-screen">
+			<div className="w-64 bg-gray-900">
+				<Sidebar onSelectConversation={setConversationID} />
+			</div>
+			<div className="flex-1 bg-gray-50">
+				Chat Window Placeholder
+			</div>
+		</div>
+	)
 }
 
 export default App
