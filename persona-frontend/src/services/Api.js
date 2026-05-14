509694e8b3f16dc7a9346c196778fe6658cc566f
Rishil Ramesh <rishil.ramesh@outlook.com>
2026-05-14 23:21:57 +0530

    Add : Helper method to get conversation messages using conversation ID from backend

--- persona-frontend/src/services/Api.js
+++ persona-frontend/src/services/Api.js
@@ -3,3 +3,9 @@ export async function getConversations() {
     const data = await response.json();
     return data;
 }
+
+export async function getConversation(conversationID) {
+    const response = await fetch(`http://localhost:8000/conversations/${conversationID}`);
+    const data = await response.json();
+    return data;
+}
