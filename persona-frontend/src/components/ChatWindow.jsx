23e7e6aff41f68265f056391d8c34f571def8d3f
Rishil Ramesh <rishil.ramesh@outlook.com>
2026-05-14 23:21:35 +0530

    Add : Skeleton structure and logic for ChatWindow component

--- persona-frontend/src/components/ChatWindow.jsx
+++ persona-frontend/src/components/ChatWindow.jsx
@@ -1,8 +1,26 @@
+import { useState, useEffect } from "react"
+import { getConversation } from "../services/Api";
+
 function ChatWindow({ conversationID }) {
+
+    const [conversation, setConversation] = useState([]);
+
+    useEffect(() => {
+        async function fetchConversation() {
+            if (!conversationID) return ;
+            const conversationDetails = await getConversation(conversationID);
+            setConversation(conversationDetails.messages)
+        }
+        fetchConversation();
+    }, [conversationID])
 
     return (
         <div>
-
+            {
+                conversation.map(message => (
+                    <div key={message.id}>{ message.content }</div>
+                ))
+            }
         </div>
     )
 }
