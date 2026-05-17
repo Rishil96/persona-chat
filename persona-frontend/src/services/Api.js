87ddbeba98389d21d4a00e95194caf3775ec770d
Rishil Ramesh <rishil.ramesh@outlook.com>
2026-05-17 16:40:27 +0530

    Add : Helper function to send user message to backend

--- persona-frontend/src/services/Api.js
+++ persona-frontend/src/services/Api.js
@@ -9,3 +9,23 @@ export async function getConversation(conversationID) {
     const data = await response.json();
     return data;
 }
+
+export async function sendMessage(conversationID, userMessage, modelName) {
+    const response = await fetch(
+        `http://localhost:8000/conversations/${conversationID}/messages`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(
+                {
+                    "user_message": userMessage,
+                    "model_name": modelName
+                }
+            )
+        }
+    )
+    const data = await response.json();
+    return data;
+}
